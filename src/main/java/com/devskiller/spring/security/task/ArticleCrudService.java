d4becad1d2f3b03bf13bafa35ff1f6bac044d095
Marcin Włodarczyk <devstructor@gmail.com>
2022-05-24 00:18:19 +0200

    partial implementation

--- src/main/java/com/devskiller/spring/security/task/ArticleCrudService.java
+++ src/main/java/com/devskiller/spring/security/task/ArticleCrudService.java
@@ -1,21 +1,26 @@
 package com.devskiller.spring.security.task;
 
-import org.springframework.stereotype.*;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.stereotype.Component;
 
-import java.util.*;
-import java.util.concurrent.*;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-import static java.util.Objects.*;
+import static java.util.Objects.requireNonNull;
 
 @Component
 public class ArticleCrudService {
     private Map<String, Article> store = new ConcurrentHashMap();
 
+    // no time ;-) sorry
+//    @PreAuthorize("#article.createdBy = authentication.principal")
     public void add(Article article) {
         requireNonNull(article);
+        // why not just set createdBy to SecurityContextHolder.getContext().getAuthentication() ?
         store.put(article.title, article);
     }
 
+    @PostAuthorize("returnObject.createdBy == authentication.principal")
     public Article get(String title) {
         requireNonNull(title);
         return store.get(title);
